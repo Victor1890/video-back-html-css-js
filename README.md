4463acdacf79cfd073b821d901702f5915e3a5c2
Victor J. Rosario V <victorrosario.1890@gmail.com>
2021-05-28 22:36:12 -0400

    add files

---
+++ README.md
@@ -0,0 +1,3 @@
+# Sample project usind HTML, CSS and some JavaScript
+
+![img](design/img.png)
---
+++ main.js
@@ -0,0 +1,9 @@
+function main() {
+  const video = document.getElementById("video");
+
+  video.src = "video.mp4";
+  video.loop = true;
+  video.autoplay = true;
+}
+
+main();
